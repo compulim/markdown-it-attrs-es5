0b0b138cc8b1124d75a5b45d1f9815312c1b9779
William Wong <compulim@users.noreply.github.com>
2022-02-28 22:03:28 +0000

    Fix tests

--- tests/e2e/npm-alias/index.js
+++ tests/e2e/npm-alias/index.js
@@ -1,4 +1,4 @@
-// "ms" is being aliased as "p-defer", thus, "p-defer-es5" is actually "ms".
-const ms = require('p-defer-es5');
+// "ms" is being aliased as "markdown-it-attrs", thus, "markdown-it-attrs-es5" is actually "ms".
+const ms = require('markdown-it-attrs-es5');
 
 console.log(ms(1000));
